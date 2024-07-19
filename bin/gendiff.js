68c561d2d96ee7734744d9286818830cef4a92d5
Nikita Pavlov <pavlovkeith@gmail.com>
2024-07-19 11:45:07 +0300

    step 1 fix

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import genDiff from '../src/index.js';
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const genDiff = (filepath1, filepath2) =>
+  // Заглушка для функции, возвращающая строку
+  // eslint-disable-next-line implicit-arrow-linebreak
+  `Comparing ${filepath1} and ${filepath2}`;
+export default genDiff;
