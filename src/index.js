1abb548050e32b6da7ed3632f27814a53e22cf2e
Nikita Pavlov <pavlovkeith@gmail.com>
2024-07-19 15:19:17 +0300

    step 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const genDiff = (filepath1, filepath2, format = 'stylish') =>
-  // Заглушка для функции, возвращающая строку
-  // eslint-disable-next-line implicit-arrow-linebreak
-  `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
+import parseFile from './parser.js';
+
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  const data1 = parseFile(filepath1);
+  const data2 = parseFile(filepath2);
+
+  // Здесь будет логика сравнения и форматирования
+  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
+};
+
 export default genDiff;
