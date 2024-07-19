b9b6db5ac4d0d0123d0f73f01fb9d872f2ffde4d
Nikita Pavlov <pavlovkeith@gmail.com>
2024-07-19 15:24:40 +0300

    step 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+import _ from 'lodash';
 import parseFile from './parser.js';
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
-  // Здесь будет логика сравнения и форматирования
-  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+
+  const result = keys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return [`  - ${key}: ${data1[key]}`, `  + ${key}: ${data2[key]}`].join(
+        '\n',
+      );
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${result.join('\n')}\n}`;
 };
 
 export default genDiff;
