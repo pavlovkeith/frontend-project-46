b74c1a13510b68c72c728691073db7df7ab57709
Nikita Pavlov <pavlovkeith@gmail.com>
2024-07-19 11:52:54 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { program } from 'commander';
 import genDiff from '../src/index.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
+  });
+
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const genDiff = (filepath1, filepath2) =>
+const genDiff = (filepath1, filepath2, format = 'stylish') =>
   // Заглушка для функции, возвращающая строку
   // eslint-disable-next-line implicit-arrow-linebreak
-  `Comparing ${filepath1} and ${filepath2}`;
+  `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
 export default genDiff;
