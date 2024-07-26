688c1cc10db8cc87df9bd7092a3609c194dbf912
Nikita Pavlov <pavlovkeith@gmail.com>
2024-07-26 12:58:54 +0300

    fid getDiff

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,33 +1,55 @@
 import _ from 'lodash';
 
+const getDiffStatus = (data1, data2, key) => {
+  const value1 = data1[key];
+  const value2 = data2[key];
+  const hasValue1 = _.has(data1, key);
+  const hasValue2 = _.has(data2, key);
+
+  if (_.isObject(value1) && _.isObject(value2)) {
+    return 'nested';
+  }
+  if (!hasValue1 && hasValue2) {
+    return 'added';
+  }
+  if (hasValue1 && !hasValue2) {
+    return 'deleted';
+  }
+  if (value1 === value2) {
+    return 'unchanged';
+  }
+  return 'changed';
+};
+
+const getDiffNode = (data1, data2, key) => {
+  const value1 = data1[key];
+  const value2 = data2[key];
+  const status = getDiffStatus(data1, data2, key);
+
+  switch (status) {
+    case 'nested':
+      // eslint-disable-next-line no-use-before-define
+      return { status, key, children: getDiffTree(value1, value2) };
+    case 'added':
+      return { status, key, value: value2 };
+    case 'deleted':
+      return { status, key, value: value1 };
+    case 'unchanged':
+      return { status, key, value: value1 };
+    case 'changed':
+      return {
+        status, key, value1, value2,
+      };
+    default:
+      throw new Error(`Unknown status: ${status}`);
+  }
+};
+
 const getDiffTree = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.sortBy(keys);
 
-  const getTree = sortedKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    const hasValue1 = _.has(data1, key);
-    const hasValue2 = _.has(data2, key);
-
-    if (_.isObject(value1) && _.isObject(value2)) {
-      return { status: 'nested', key, children: getDiffTree(value1, value2) };
-    }
-    if (!hasValue1 && hasValue2) {
-      return { status: 'added', key, value: value2 };
-    }
-    if (hasValue1 && !hasValue2) {
-      return { status: 'deleted', key, value: value1 };
-    }
-    if (value1 === value2) {
-      return { status: 'unchanged', key, value: value1 };
-    }
-    return {
-      status: 'changed', key, value1, value2,
-    };
-  });
-
-  return getTree;
+  return sortedKeys.map((key) => getDiffNode(data1, data2, key));
 };
 
 export default getDiffTree;
