5f19ca013902ae5bb4e6f4e4e6e5c8fed399d1e0
Nikita Pavlov <pavlovkeith@gmail.com>
2024-07-26 12:51:46 +0300

    fix  getDiff

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,47 +1,33 @@
 import _ from 'lodash';
 
-const getDiffStatus = (data1, data2, key) => {
-  if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-    return 'nested';
-  }
-  if (!(_.has(data1, key)) && _.has(data2, key)) {
-    return 'added';
-  }
-  if (_.has(data1, key) && !(_.has(data2, key))) {
-    return 'deleted';
-  }
-  if (data1[key] === data2[key]) {
-    return 'unchanged';
-  }
-  return 'changed';
-};
-
-const getValue = (data1, data2, key, status) => {
-  switch (status) {
-    case 'nested':
-      // eslint-disable-next-line no-use-before-define
-      return { children: getDiffTree(data1[key], data2[key]) };
-    case 'added':
-      return { value: data2[key] };
-    case 'deleted':
-      return { value: data1[key] };
-    case 'unchanged':
-      return { value: data1[key] };
-    case 'changed':
-      return { value1: data1[key], value2: data2[key] };
-    default:
-      throw new Error(`Unknown status: ${status}`);
-  }
-};
-
 const getDiffTree = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.sortBy(keys);
 
-  return sortedKeys.map((key) => {
-    const status = getDiffStatus(data1, data2, key);
-    return { status, key, ...getValue(data1, data2, key, status) };
+  const getTree = sortedKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    const hasValue1 = _.has(data1, key);
+    const hasValue2 = _.has(data2, key);
+
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { status: 'nested', key, children: getDiffTree(value1, value2) };
+    }
+    if (!hasValue1 && hasValue2) {
+      return { status: 'added', key, value: value2 };
+    }
+    if (hasValue1 && !hasValue2) {
+      return { status: 'deleted', key, value: value1 };
+    }
+    if (value1 === value2) {
+      return { status: 'unchanged', key, value: value1 };
+    }
+    return {
+      status: 'changed', key, value1, value2,
+    };
   });
+
+  return getTree;
 };
 
 export default getDiffTree;
